7dc9bc2aefa678446fdb319a44772b68ef2a45ba
David Díaz <201807420daviddiaz@gmail.com>
2020-09-09 08:29:09 -0600

    Barra de Navegacion

--- proyecto 1/Principal.py
+++ proyecto 1/Principal.py
@@ -4,8 +4,21 @@ class Interfaz:
     def __init__(self,ventana):
         self.ventana=ventana
         self.ventana.title("[OLC1]Proyecto1")
-        
-        self.menu=Menu()
+        #-------------------------Barra de navegacion
+        self.menu=Menu(self.ventana)
+        new_item=Menu(self.menu)
+        new_item.add_command(label='Nuevo')
+        new_item.add_command(label='Abrir')
+        new_item.add_command(label='Guardar')
+        new_item.add_command(label='Guardar Como...')
+        new_item.add_command(label='Analizar')
+        new_item.add_separator()
+        new_item.add_command(label='Salir')
+        self.menu.add_cascade(label='Archivo',menu=new_item)
+        self.menu.add_cascade(label='Ayuda')
+        self.ventana.config(menu=self.menu)
+        #-------------------------------------Panel donde estara todo
+
 
 Ventana_principal=Tk()
 Principal=Interfaz(Ventana_principal)
@@ -13,31 +26,3 @@ Ventana_principal.mainloop()
 
 
 
-'''
-raiz=Tk() #inicia la ventana
-#-------------------------------Ventana
-raiz.title("Principal")
-#--------------------------------Menu de navegacion------------------------
-menu=Menu(raiz)
-new_item = Menu(menu)
-new_item.add_command(label='Nuevo')
-new_item.add_command(label='Abrir')
-new_item.add_command(label='Guardar como...')
-new_item.add_command(label='Analizar')
-new_item.add_separator()
-new_item.add_command(label='Salir')
-menu.add_cascade(label='Archivo',menu=new_item)
-menu.add_cascade(label='Ayuda')
-
-raiz.config(menu=menu)
-
-#-------------------------------Contenedor Principal-------------------
-contenedor1=Frame()
-contenedor1.pack()    #empaquetar(meterlo dentro de la ventana)
-contenedor1.config(width="900", height="600")
-
-
-
-raiz.mainloop() #bucle infinito para que la ventana este en ejecucion continua
-
-##'''
