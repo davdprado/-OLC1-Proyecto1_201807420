a8a5f29775071c9892bd4f42ede24ab556161cd8
David Díaz <201807420daviddiaz@gmail.com>
2020-09-12 02:22:12 -0600

    Abrir archivo e insertar

---
+++ proyecto 1/Error.py
@@ -0,0 +1,7 @@
+class Error():
+    def __init__(self,id,no,fila,columna,desc):
+        self.Id=id
+        self.No=no
+        self.Fila=fila
+        self.Columna=columna
+        self.Descripcion=desc
--- proyecto 1/Principal.py
+++ proyecto 1/Principal.py
@@ -2,38 +2,50 @@ from tkinter import *
 from tkinter import filedialog
 from tkinter import scrolledtext
 
-def abrir_archivo():
-    archivo_abierto=filedialog.askopenfilename( title='Abrir Archivo',filetypes=(("css files",".css"),("all filse",".")))
-    print(archivo_abierto)
-class Interfaz:
-    def __init__(self,ventana):
-        self.ventana=ventana
-        self.ventana.title("[OLC1]Proyecto1")
-        #-------------------------Barra de navegacion
-        self.menu=Menu(self.ventana)
-        new_item=Menu(self.menu)
-        new_item.add_command(label='Nuevo')
-        new_item.add_command(label='Abrir',command=abrir_archivo)
-        new_item.add_command(label='Guardar')
-        new_item.add_command(label='Guardar Como...')
-        new_item.add_command(label='Analizar')
-        new_item.add_separator()
-        new_item.add_command(label='Salir')
-        self.menu.add_cascade(label='Archivo',menu=new_item)
-        self.menu.add_cascade(label='Ayuda')
-        self.ventana.config(menu=self.menu)
-        #-------------------------------------Panel donde estara todo
-        frame = LabelFrame(self.ventana, text = 'Analizador Lexico')
-        frame.grid(row=0,column=0,columnspan=8,pady=20)
 
-        txt = Text(frame,width=70,height=30)
-        txt.grid(column=0,row=0)
-        
-        
+def abrir_archivo():
+    archivo_abierto=filedialog.askopenfilename( title='Abrir Archivo', initialdir='/home/roberto/',filetypes=(("css files",".css"),("all filse",".")))
+    ruta=archivo_abierto
+    if ruta !="":
+        arch=open(ruta,'r')
+        lectura=arch.read()
+        text1.insert("1.0",lectura)
+        arch.close()
+    
+    
 
 
 Ventana_principal=Tk()
-Principal=Interfaz(Ventana_principal)
+Ventana_principal.title("[OLC1]Proyecto1")
+
+#-------------------------Barra de navegacion
+menu=Menu(Ventana_principal)
+new_item=Menu(menu)
+new_item.add_command(label='Nuevo')
+new_item.add_command(label='Abrir',command=abrir_archivo)
+new_item.add_command(label='Guardar')
+new_item.add_command(label='Guardar Como...')
+new_item.add_command(label='Analizar')
+new_item.add_separator()
+new_item.add_command(label='Salir')
+menu.add_cascade(label='Archivo',menu=new_item)
+menu.add_cascade(label='Ayuda')
+Ventana_principal.config(menu=menu)
+#-------------------------------------Panel donde estara todo
+frame = LabelFrame(Ventana_principal, text = 'Analizador Lexico')
+frame.grid(row=0,column=0,columnspan=8,pady=20)
+
+text1 = Text(frame,width=70,height=30)
+text1.grid(column=0,row=0)
+        
+        
+        
+        
+  
+        
+
+
+
 Ventana_principal.mainloop()
 
 
