c65ec12b8948983cee8207a9141cbcba16f18771
David Díaz <201807420daviddiaz@gmail.com>
2020-09-11 21:25:47 -0600

    Abrir Archivo

--- proyecto 1/Principal.py
+++ proyecto 1/Principal.py
@@ -1,5 +1,10 @@
 from tkinter import *
+from tkinter import filedialog
+from tkinter import scrolledtext
 
+def abrir_archivo():
+    archivo_abierto=filedialog.askopenfilename( title='Abrir Archivo',filetypes=(("css files",".css"),("all filse",".")))
+    print(archivo_abierto)
 class Interfaz:
     def __init__(self,ventana):
         self.ventana=ventana
@@ -8,7 +13,7 @@ class Interfaz:
         self.menu=Menu(self.ventana)
         new_item=Menu(self.menu)
         new_item.add_command(label='Nuevo')
-        new_item.add_command(label='Abrir')
+        new_item.add_command(label='Abrir',command=abrir_archivo)
         new_item.add_command(label='Guardar')
         new_item.add_command(label='Guardar Como...')
         new_item.add_command(label='Analizar')
@@ -18,6 +23,13 @@ class Interfaz:
         self.menu.add_cascade(label='Ayuda')
         self.ventana.config(menu=self.menu)
         #-------------------------------------Panel donde estara todo
+        frame = LabelFrame(self.ventana, text = 'Analizador Lexico')
+        frame.grid(row=0,column=0,columnspan=8,pady=20)
+
+        txt = Text(frame,width=70,height=30)
+        txt.grid(column=0,row=0)
+        
+        
 
 
 Ventana_principal=Tk()
